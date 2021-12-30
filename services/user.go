285034a6480b8c59f5239c9819790cfe58cf7a43
MatheusR42 <matheus.araujo@mercadolivre.com>
2021-12-29 22:53:51 -0300

    grpc server

---
+++ services/user.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"context"
+
+	"github.com/matheusr42/go-grpc/pb/pb"
+)
+
+// type UserServiceServer interface {
+// 	AddUser(context.Context, *User) (*User, error)
+// 	mustEmbedUnimplementedUserServiceServer()
+// }
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func NewUserService() *UserService {
+	return &UserService{}
+}
+
+func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
+	// inserting to db
+
+	// returning
+	return &pb.User{
+		Id:    "123",
+		Name:  nil,
+		Email: req.GetEmail(),
+	}, nil
+}
